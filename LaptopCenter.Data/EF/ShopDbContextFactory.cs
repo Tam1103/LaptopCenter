4f562b1b4bf9d3b0fabf4e47cf0d989d123db839
tam1103 <minhtam110397@gmail.com>
2020-09-26 18:19:09 +0700

    complete migrition to sql server

---
+++ LaptopCenter.Data/EF/ShopDbContextFactory.cs
@@ -0,0 +1,29 @@
+﻿using LaptopCenter.Data.EF;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace eShopSolution.Data.EF
+{
+    public class ShopDbContextFactory : IDesignTimeDbContextFactory<ShopDbContext>
+    {
+        public ShopDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var connectionString = configuration.GetConnectionString("LaptopShop");
+
+            var optionsBuilder = new DbContextOptionsBuilder<ShopDbContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return new ShopDbContext(optionsBuilder.Options);
+        }
+    }
+}
